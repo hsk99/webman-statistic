9121eee4b38a034a67e30d6b69401581fc06295a
hsk99 <3024186605@qq.com>
2022-01-13 11:50:04 +0800

    update

--- src/Install.php
+++ src/Install.php
@@ -42,15 +42,11 @@ class Install
                 $parent_dir = base_path() . '/' . substr($dest, 0, $pos);
                 if (!is_dir($parent_dir)) {
                     mkdir($parent_dir, 0777, true);
-                } else {
-                    continue;
                 }
             }
-            if (is_file(base_path() . "/$dest")) {
-                continue;
-            }
             //symlink(__DIR__ . "/$source", base_path()."/$dest");
-            copy_dir(__DIR__ . "/$source", base_path() . "/$dest");
+            // copy_dir(__DIR__ . "/$source", base_path() . "/$dest");
+            static::copyDir(__DIR__ . "/$source", base_path() . "/$dest");
         }
     }
 
@@ -69,6 +65,34 @@ class Install
                 unlink($path);
             }*/
             remove_dir($path);
+        }
+    }
+
+    /**
+     * 拷贝文件，存在不覆盖
+     *
+     * @author HSK
+     * @date 2022-01-13 11:47:17
+     *
+     * @param string $source
+     * @param string $dest
+     *
+     * @return void
+     */
+    protected static function copyDir($source, $dest)
+    {
+        if (is_dir($source)) {
+            if (!is_dir($dest)) {
+                mkdir($dest);
+            }
+            $files = scandir($source);
+            foreach ($files as $file) {
+                if ($file !== "." && $file !== "..") {
+                    static::copyDir("$source/$file", "$dest/$file");
+                }
+            }
+        } else if (file_exists($source) && !is_file($dest)) {
+            copy($source, $dest);
         }
     }
 }
