2cc5537e8dd6b8b5f0994ecde21c02535dca5705
hsk99 <3024186605@qq.com>
2022-08-31 13:54:52 +0800

    update

--- src/config/plugin/hsk99/statistic/app.php
+++ src/config/plugin/hsk99/statistic/app.php
@@ -6,5 +6,5 @@ return [
     'interval'       => 30,                                                  // 上报间隔
     'address'        => 'http://127.0.0.1:8788/report/statistic/transfer',   // 上报地址
     'authorization'  => null,                                                // 上报认证key，默认 null
-    'global_monitor' => true,                                                // 是否执行全局监听SQL、Redis
+    'global_monitor' => true,                                                // 是否单独监听上报所有进程SQL、Redis
 ];
